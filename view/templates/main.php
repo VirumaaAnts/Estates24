85743f6d0ecb1dea0d0be9a13af22e06f36b6f31
Max <90319998+9I6JloKo@users.noreply.github.com>
2023-02-23 09:44:12 +0200

    header

--- view/templates/main.php
+++ view/templates/main.php
@@ -5,29 +5,48 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="public/css/style.css">
-    <link rel="stylesheet" href="public/css/table.css">
-    <link rel="stylesheet" href="public/css/loginLogout.css">
-    <link rel="stylesheet" href="public/css/insert.css">
-    <link rel="stylesheet" href="public/css/update.css">
-    <link rel="stylesheet" href="public/css/delete.css">
+    <link rel="stylesheet" href="public/css/header.css">
     <script src="public/js/app.js"></script>
-    <title>People</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
+    <title>Estates24/7</title>
 </head>
 <body>
     <header>
-        <h1>People</h1>
-        <?php
-            if(isset($_SESSION['status'])){
-                echo '<p style="text-align:center;text-transform:uppercase">Signed as<br><b>'.$_SESSION['role'].'</b></p>';
-            }
-        ?>
-        <div>
+
+        <div class="logo">
+
+            <div class="imgHeader">
+                <img src="images/621b8ec9f9fd4955bc17fb7fa56d19a4 (1).avif" alt="">
+            </div>
+
+            <h1>Estates 24/7</h1>
+
+        </div>
+
+        <div class="buttonContainer">
+
+            <div class="linksContainer">
+                <a href="macklers">Маклеры</a>
+            </div>
+
+            <div class="favouritesContainer">
+                <a href="favourites"><img src="images/pngimg.com - like_PNG61.png" alt=""></a>
+            </div>
+
+            <a href="addAdv">Добавить объявление</a>
+
             <?php
                 if(isset($_SESSION['status'])){
-                    echo '<a class="logout" href="logout">logout</a>';
+                    
+                }else{
+                    echo '<div class="loginContainer"><a class="login" href="login">Войти</a></div>';
                 }
             ?>
+
         </div>
+
     </header>
     <main>
         <?php

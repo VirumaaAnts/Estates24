8b5e5074a30f03a59283f7b037adcc008c2c8af0
Maksim Dzjubenko <90320393+SvinDzjuba@users.noreply.github.com>
2023-03-14 11:26:01 +0200

    bb

--- view/registration.php
+++ view/registration.php
@@ -19,7 +19,7 @@
         <div>
             <input type="text" class="form-control" name="username" required placeholder="Username"  value=<?php if(isset($data[2][3])){echo '"'.$data[2][3].'"';}else { echo '""';};?> />
             <input type="password" class="form-control" name="password" required placeholder="Password"  value= ""  />
-            <input type="tel"class="form-control" name="phone" required placeholder="Phone"  value=<?php if(isset($data[2][6])){echo '"'.$data[2][6].'"';}else { echo '""';};?>  />
+            <input type="tel" class="form-control" pattern="[0-9]{7,9}" name="phone" required placeholder="Phone" value=<?php if(isset($data[2][6])){echo '"'.$data[2][6].'"';}else { echo '""';};?>  />
             <input type="text" class="form-control" name="mackler" disabled placeholder="You don't have a mackler status" />
         </div>
         
